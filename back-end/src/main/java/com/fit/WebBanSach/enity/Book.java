046ae14696158e0483cf069cdf8db97ae939c286
Trần Tấn Lộc <20130312@st.hcmuaf.edu.vn>
2024-11-03 05:01:52 +0700

    create BE book

---
+++ back-end/src/main/java/com/fit/WebBanSach/enity/Book.java
@@ -0,0 +1,12 @@
+package com.fit.WebBanSach.enity;
+
+public class Book {
+    private int idBook;
+    private String nameBook;
+    private String nameAuthor;
+    private String description;
+    private double price;
+    private double listPrice;
+
+
+}
