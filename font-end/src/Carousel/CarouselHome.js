597134e1168debb7e7dbb3fc9488ec6990b58da5
Trần Tấn Lộc <20130312@st.hcmuaf.edu.vn>
2024-11-22 12:16:00 +0700

    add cdn bootstrap

--- font-end/src/Carousel/CarouselHome.js
+++ font-end/src/Carousel/CarouselHome.js
@@ -1,31 +1,39 @@
 import Carousel from 'react-bootstrap/Carousel';
-// import ExampleCarouselImage from 'components/ExampleCarouselImage';
-import Imgae1 from '../imgs/bookslide.jpg';
-import Imgae2 from '../imgs/logo-web.png';
-import Imgae3 from '../imgs/showPass.png';
-
+import IMG from '../imgs/bookslide.jpg'
 
 function CarouselHome() {
   return (
-    <Carousel>
-      <Carousel.Item interval={1000}>
-        <Imgae1/>
+    <Carousel data-bs-theme="dark">
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={IMG}
+          alt="First slide"
+        />
         <Carousel.Caption>
-          <h3>First slide label</h3>
+          <h5>First slide label</h5>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item interval={500}>
-        <Imgae2/>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={IMG}
+          alt="Second slide"
+        />
         <Carousel.Caption>
-          <h3>Second slide label</h3>
+          <h5>Second slide label</h5>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <Imgae3/>
+        <img
+          className="d-block w-100"
+          src={IMG}
+          alt="Third slide"
+        />
         <Carousel.Caption>
-          <h3>Third slide label</h3>
+          <h5>Third slide label</h5>
           <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
           </p>
